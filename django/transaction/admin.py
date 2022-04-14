4b951d955fcf4bf4ec6e65ea2db496428d5b9b52
Santhoshkumard11 <santhoshkdhana@gmail.com>
2022-04-14 18:37:04 +0530

    working - added new models and db

--- django/transaction/admin.py
+++ django/transaction/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, BankAccount, PaymentRequest, BankBranch, Transaction
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "is_active", "account_number")
+
+
+@admin.register(PaymentRequest)
+class PaymentRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "receiver_name",
+        "sender_name",
+        "request_initiated_type",
+    )
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "account_number")
+    exclude = ("amount",)
+
+
+@admin.register(BankBranch)
+class BankBranchAdmin(admin.ModelAdmin):
+    list_display = ("name", "ifse_code")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "transaction_id",
+        "account_number",
+        "amount",
+        "status",
+    )
--- django/transaction/models.py
+++ django/transaction/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from string import digits
+import random
 from transaction.constants import (
     ACCOUNT_STATUS_CHOICES,
     PAYMENT_REQUEST_STATUS_CHOICES,
     REQUEST_INITIATED_TYPE_CHOICES,
     TRANSACTION_STATUS_CHOICES,
 )
+
+
+def generate_numbers(size=4):
+    return "".join(random.choice(digits) for _ in range(size))
+
+
+def generate_account_number():
+    return f"1{generate_numbers()[1:]}-" + "-".join([generate_numbers() for _ in range(3)])
 
 
 class PaymentRequest(models.Model):
@@ -14,24 +24,29 @@ class PaymentRequest(models.Model):
     )
     receiver_name = models.CharField(max_length=50)
     sender_name = models.CharField(max_length=50)
-    status = models.CharField(choices=PAYMENT_REQUEST_STATUS_CHOICES, max_length=40, default="pending")
+    status = models.CharField(
+        choices=PAYMENT_REQUEST_STATUS_CHOICES, max_length=40, default="pending"
+    )
 
 
 class BankAccount(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    account_number = models.CharField(max_length=18, unique=True)
-    amount = models.DecimalField(decimal_places=2, max_digits=30)
+    account_number = models.CharField(
+        max_length=19, unique=True, default=generate_account_number()
+    )
+    amount = models.DecimalField(decimal_places=2, max_digits=30, default=0.0)
 
     def __str__(self) -> str:
         return f"{self.account_number}"
 
 
-class Bank(models.Model):
+class BankBranch(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=50, unique=True, help_text="Name of the bank")
-    address = models.TextField(null=True, blank=True, help_text="Address of the bank")
+    name = models.CharField(max_length=50, unique=True)
+    ifse_code = models.CharField(max_length=15)
+    address = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -49,20 +64,9 @@ class Customer(models.Model):
         BankAccount, to_field="account_number", on_delete=models.CASCADE
     )
 
-    bank_name = models.ForeignKey(Bank, to_field="name", on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return f"{self.name}"
-
-
-class BankBranch(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=50, unique=True)
-    ifse_code = models.CharField(max_length=15)
-    address = models.TextField(null=True, blank=True)
-
-    bank_name = models.ForeignKey(Bank, to_field="name", on_delete=models.CASCADE)
+    branch_name = models.ForeignKey(
+        BankBranch, to_field="name", on_delete=models.CASCADE
+    )
 
     def __str__(self) -> str:
         return f"{self.name}"
@@ -73,16 +77,15 @@ class Transaction(models.Model):
     transaction_id = models.CharField(max_length=50)
     receiver_name = models.CharField(max_length=50)
     sender_name = models.CharField(max_length=50)
-    bank_name = models.ForeignKey(Bank, to_field="name", on_delete=models.CASCADE)
-    branch_name = models.ForeignKey(
-        BankBranch, to_field="name", on_delete=models.CASCADE
-    )
+
     account_number = models.ForeignKey(
         BankAccount, to_field="account_number", on_delete=models.CASCADE
     )
     amount = models.DecimalField(max_digits=20, decimal_places=2)
 
-    status = models.CharField(choices=TRANSACTION_STATUS_CHOICES, max_length=40, default="pending")
+    status = models.CharField(
+        choices=TRANSACTION_STATUS_CHOICES, max_length=40, default="pending"
+    )
 
     def __str__(self) -> str:
         return f"{self.transaction_id}"
